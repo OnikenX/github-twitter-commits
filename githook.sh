09394311dd892102e7c6c9fac80a4f3af14197a7
OnikenX <OnikenX@protonmail.com>
2020-07-29 06:01:00 +0100

    revision

--- githook.sh
+++ githook.sh
@@ -9,10 +9,8 @@ if [[ $1 == "push" ]]
 then
 	if [[ $(expr $(date +%s) - $(git log -1 --date=raw | tail -n +3 | replace ' ' '' | replace Date: '' | cut -d '+' -f -1 | head -n 1)) > 60 ]]
 	then
-		echo maior que 60 segundos
+		echo took longer than 60 seconds
 		break
-	else
-		echo dentro do minuto
 	fi
 echo Commited:\"$(git log -1 | tail -n +5)\" in $(git remote get-url origin) | tweet
 fi
